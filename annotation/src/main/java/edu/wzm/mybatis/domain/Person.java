d960557ccf47b080e87a9a56983388581bca707f
GatsbyNewton <wangzmking@163.com>
2017-07-17 00:21:13 +0800

    Java Bean (POJO)

---
+++ annotation/src/main/java/edu/wzm/mybatis/domain/Person.java
@@ -0,0 +1,51 @@
+package edu.wzm.mybatis.domain;
+
+import org.apache.ibatis.type.Alias;
+
+/**
+ * Created by gatsbynewton on 2017/7/15.
+ */
+@Alias("Person")
+public class Person {
+
+    private int id;
+    private String name;
+    private int age;
+
+    public Person() {
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person: [id=" + id + ", name=" + name + ", age=" + age + "]";
+    }
+}
---
+++ configuration/src/main/java/edu/wzm/mybatis/domain/Person.java
@@ -0,0 +1,48 @@
+package edu.wzm.mybatis.domain;
+
+/**
+ * Created by gatsbynewton on 2017/7/15.
+ */
+public class Person {
+
+    private int id;
+    private String name;
+    private int age;
+
+    public Person() {
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person: [name=" + name + ", age=" + age + "]";
+    }
+}
