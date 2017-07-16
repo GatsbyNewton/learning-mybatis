d2b0d7ca2ed55dfb803c1b1f0d33ce80bcea1960
GatsbyNewton <wangzmking@163.com>
2017-07-17 00:22:11 +0800

    MyBatis Mapping Files

---
+++ annotation/src/main/java/edu/wzm/mybatis/mapping/PersonMapper.java
@@ -0,0 +1,33 @@
+package edu.wzm.mybatis.mapping;
+
+import edu.wzm.mybatis.domain.Person;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import java.util.List;
+
+/**
+ * Created by gatsbynewton on 2017/7/15.
+ */
+public interface PersonMapper {
+
+    @Insert("insert into person(name, age) values(#{name}, #{age})")
+    public int add(Person person);
+
+    @Select("select * " +
+            "from person " +
+            "where id = #{id}")
+    public Person getById(int id);
+
+    @Select("select * from person")
+    public List<Person> getAll();
+
+    @Update("update person set age = #{age} where id = #{id}")
+    public int update(Person person);
+
+    @Delete("delete from person where id = #{id}")
+    public int deleteById(int id);
+
+}
