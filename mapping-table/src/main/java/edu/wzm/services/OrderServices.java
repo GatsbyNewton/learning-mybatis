dc6535d5d40b272f5f980db3031a53b3852b7a7c
GatsbyNewton <wangzmking@163.com>
2017-08-15 23:50:23 +0800

    service class

---
+++ mapping-table/src/main/java/edu/wzm/services/OrderServices.java
@@ -0,0 +1,41 @@
+package edu.wzm.services;
+
+import edu.wzm.mybatis.domain.Order;
+import edu.wzm.tools.MyBatisUtils;
+import org.apache.ibatis.session.SqlSession;
+
+/**
+ * Created by gatsbynewton on 2017/8/15.
+ */
+public class OrderServices {
+
+    public void add(){
+        Order order = new Order("QEAVRWE", 98.6f);
+        SqlSession sqlSession = MyBatisUtils.getSqlSession(true);
+
+        int result = sqlSession.insert("edu.wzm.mybatis.mapping.orderMapper.insert", order);
+        System.out.println(result);
+        sqlSession.close();
+    }
+
+    public void queryWithNothing(){
+        SqlSession sqlSession = MyBatisUtils.getSqlSession();
+        Order order = sqlSession.selectOne("edu.wzm.mybatis.mapping.orderMapper.selectWithNothing", 1);
+        System.out.println(order);
+        sqlSession.close();
+    }
+
+    public void queryWithAlias(){
+        SqlSession sqlSession = MyBatisUtils.getSqlSession();
+        Order order = sqlSession.selectOne("edu.wzm.mybatis.mapping.orderMapper.selectWithAlias", 1);
+        System.out.println(order);
+        sqlSession.close();
+    }
+
+    public void queryWithMapping(){
+        SqlSession sqlSession = MyBatisUtils.getSqlSession();
+        Order order = sqlSession.selectOne("edu.wzm.mybatis.mapping.orderMapper.selectWithMapping", 1);
+        System.out.println(order);
+        sqlSession.close();
+    }
+}
