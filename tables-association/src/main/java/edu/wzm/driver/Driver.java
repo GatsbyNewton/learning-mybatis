42d5a590061f4d07fa2e1140d47c71dffa28fc56
GatsbyNewton <wangzmking@163.com>
2017-08-16 23:20:07 +0800

    Main class

---
+++ tables-association/src/main/java/edu/wzm/driver/Driver.java
@@ -0,0 +1,15 @@
+package edu.wzm.driver;
+
+import edu.wzm.services.AssociationService;
+
+/**
+ * Created by gatsbynewton on 2017/8/16.
+ */
+public class Driver {
+
+    public static void main(String[] args){
+        AssociationService service = new AssociationService();
+        service.queryOneToOne();
+        service.queryOneToMany();
+    }
+}
